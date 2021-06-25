01f1a89a6998bfaa023a5c77eb6ad9cf351ec414
lindomarc <lindomarc@outlook.com>
2021-06-25 17:27:17 -0300

    feat: sidebar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import * as FavIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
 import { Link } from "react-router-dom";
+import { SideBarData } from "./SidebarData";
+import "./Navbar.css";
+import { IconContext } from "react-icons";
 
 function Navbar() {
-  return (
+
+  const [sidebar, setSidebar] = useState(false);
+
+  const showSidebar = () => setSidebar(!sidebar);
+
+  return ( 
     <>
-      <div className="navbar">
-          <Link to="#" className='menu-bars'></Link>
-      </div>
+      <IconContext.Provider value={{color: '#fffff' }}>
+        <div className="navbar">
+          <Link to="#" className="menu-bars">
+            <FavIcons.FaBars onClick={showSidebar} />
+          </Link>
+        </div>
+        <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
+          <ul className="nav-menu-items"  onClick={showSidebar}>
+            <li className="navbar-toggle">
+              <Link to="#" className="menu-bars">
+                <AiIcons.AiOutlineClose />
+              </Link>
+            </li>
+            {SideBarData.map((item, index) => {
+              return (
+                <li key={index} className={item.cName}>
+                  <Link to={item.path}>
+                    {item.icon}
+                    <span>{item.title}</span>
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </IconContext.Provider>
     </>
   );
 }
---
+++ src/components/SidebarData.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import * as  FavIcons from "react-icons/fa";
+import * as  AiIcons from "react-icons/ai";
+import * as  IoIcons from "react-icons/io";
+
+export const SideBarData = [
+    {
+        title: 'Home',
+        path: '/',
+        icon: <AiIcons.AiFillHome />,
+        cName: 'nav-text',
+    },
+    {
+        title: 'Home',
+        path: '/',
+        icon: <AiIcons.AiFillHome />,
+        cName: 'nav-text',
+    },
+    {
+        title: 'Reports',
+        path: '/reports',
+        icon: <IoIcons.IoIosPaper  />,
+        cName: 'nav-text',
+    },
+    {
+        title: 'Products',
+        path: '/products',
+        icon: <FavIcons.FaCartPlus />,
+        cName: 'nav-text',
+    },
+    {
+        title: 'Messages',
+        path: '/',
+        icon: <FavIcons.FaEnvelopeOpenText />,
+        cName: 'nav-text',
+    },
+    {
+        title: 'Suport',
+        path: '/suport',
+        icon: <IoIcons.IoMdHelpCircle />,
+        cName: 'nav-text',
+    },
+    
+]
